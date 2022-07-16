4dcf63fbeed7b7254e944c23dc86fd1bdb074e53
Gökhan Kurtuluş <gokhankurtulus@icloud.com>
2022-07-16 11:33:59 +0300

    beforeAfter calculation fixed, unnecessary for loop removed

--- Pagination/Pagination.php
+++ Pagination/Pagination.php
@@ -5,11 +5,11 @@ class Pagination
 {
 
     /**
-     * @param int $currentIndex         current page number
-     * @param int $itemsPerPage         how many items showing per page
-     * @param int $totalItems           total items
-     * @param int $maxPaginationItems   how many list item you want to see
-     * @param string $link              links for page numbers
+     * @param int $currentIndex current page number
+     * @param int $itemsPerPage how many items showing per page
+     * @param int $totalItems total items
+     * @param int $maxPaginationItems how many list item you want to see
+     * @param string $link links for page numbers
      * @return string                   returns list
      */
     public static function paginate(int $currentIndex, int $itemsPerPage, int $totalItems, int $maxPaginationItems, string $link)
@@ -17,17 +17,18 @@ class Pagination
         // Start and end of the list
         $firstPage = 1;
         $lastPage = ceil($totalItems / $itemsPerPage);  // Calculate total page
-        // Calculate half of the list for number of left and right elements
-        $beforeAfter = floor($maxPaginationItems / 2);
 
         if ($currentIndex > $lastPage)  // Current index cannot be bigger than maximum page
             $currentIndex = $lastPage;
 
-        if ($maxPaginationItems > $lastPage) { // Page list can't be bigger than maximum page and check do we need prev and next links
+        if ($maxPaginationItems >= $lastPage) { // Page list can't be bigger than maximum page and check do we need prev and next links
             $maxPaginationItems = $lastPage;
             $prevAndNext = false;
         } else
             $prevAndNext = true;
+
+        // Calculate half of the list for number of left and right elements
+        $beforeAfter = floor($maxPaginationItems / 2);
 
         // If the current page at the starting of the list
         if ($currentIndex <= $maxPaginationItems - $beforeAfter) {
@@ -58,15 +59,15 @@ class Pagination
 
         // Assign html elements to variable for return list completely
         $list = '<ul class="pagination">';
-        if ($prevAndNext)                                               // Go first page
+        if ($prevAndNext)                                                       // Go first page
             $list .= "<li><a href='$link$firstPage'>&lt;</a></li>";
-        for ($i = $before; $i < $currentIndex; $i++)                    // Before current page
-            $list .= "<li><a href='$link$i'>$i</a></li>";
-        $list .= "<li><a class='active' href='$link$i'>$i</a></li>";    // Current page
-        for ($i = $currentIndex + 1; $i <= $after; $i++)                // After current page
-            $list .= "<li><a href='$link$i'>$i</a></li>";
-
-        if ($prevAndNext)                                               // Go last page
+        for ($i = $before; $i <= $after; $i++) {
+            if ($i == $currentIndex)
+                $list .= "<li><a class='active' href='$link$i'>$i</a></li>";    // Current Page
+            else
+                $list .= "<li><a href='$link$i'>$i</a></li>";                   // Other Pages
+        }
+        if ($prevAndNext)                                                       // Go last page
             $list .= "<li><a href='$link$lastPage'>&gt;</a></li>";
         $list .= '</ul>';
 
